6b525485e046f27ba1fc50e271d637e3b18e9f35
natakaro <natakaro@gmail.com>
2017-05-25 15:36:53 +0200

    poprawki

---
+++ Source/RuntimeMeshTerrain/Private/MyClass.cpp
@@ -0,0 +1,12 @@
+// Copyright 2016 Andreas Schoch (aka Minaosis). All Rights Reserved.
+
+#include "RuntimeMeshTerrain.h"
+#include "MyClass.h"
+
+MyClass::MyClass()
+{
+}
+
+MyClass::~MyClass()
+{
+}
---
+++ Source/RuntimeMeshTerrain/Public/MyClass.h
@@ -0,0 +1,13 @@
+// Copyright 2016 Andreas Schoch (aka Minaosis). All Rights Reserved.
+
+#pragma once
+
+/**
+ * 
+ */
+class RUNTIMEMESHTERRAIN_API MyClass
+{
+public:
+	MyClass();
+	~MyClass();
+};
